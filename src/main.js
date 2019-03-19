9b7b1468b9a6a40a7b9679b76b0cdf81a16a141e
nestorovicevo <nestorovicevo@gmail.com>
2019-03-19 18:56:17 +0100

    prvi zadatak, treba da se doradi

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+
+import AppCars from './components/AppCars'
+
+const routes = [
+  {path: '/', component: AppCars, redirect: '/cars'}
+]
+
+const router = new VueRouter({
+  router: routes,
+  mode: 'history'
+})
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
